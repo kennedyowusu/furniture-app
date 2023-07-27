c9f86ba589bdc58b1f356446d6d744fd5a1156fd
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-27 06:40:48 +0000

    remove these files

--- lib/constants/data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xFF548aca);
-
-const List<Map<String, Object>> cart = [
-  {'image': 'assets/images/wardrobe.png'},
-  {'image': 'assets/images/light-brown.png'},
-  {'image': 'assets/images/wardrobe-brown.png'},
-];
